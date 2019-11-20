0a6a630a6379d4a3b2a4e83142e2e5ae021bbfaf
jackyoh <jack@is-land.com.tw>
2019-11-20 10:08:58 +0800

    Modify consumer service

--- service-consumer/src/main/java/com/example/serviceconsumer/RibbonClient.java
+++ service-consumer/src/main/java/com/example/serviceconsumer/RibbonClient.java
@@ -3,12 +3,9 @@ package com.example.serviceconsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
 
 @RestController
 public class RibbonClient {
@@ -16,22 +13,13 @@ public class RibbonClient {
   private LoadBalancerClient lba;
 
   @RequestMapping("/test")
-  public String getGreetings() throws Exception {
-      ServiceInstance serviceInstance = lba.choose("service-provider");
+  public String test() {
+    ServiceInstance serviceInstance = lba.choose("service-provider");
 
-      String baseUrl = serviceInstance.getUri().toString();
-      baseUrl = baseUrl + "/hello";
+    String baseUrl = serviceInstance.getUri() + "/hello";
+    System.out.println(baseUrl);
 
-      System.out.println(baseUrl);
-
-      RestTemplate restTemplate = new RestTemplate();
-      ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(), String.class);
-      return response.getBody();
+    RestTemplate restTemplate = new RestTemplate();
+    return restTemplate.getForObject(baseUrl, String.class);
   }
-
-    private static HttpEntity<?> getHeaders() throws IOException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-        return new HttpEntity<>(headers);
-    }
 }
